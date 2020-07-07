53547f8f90d0034965e84140b21f935988eaabe7
ibbocus <ib_bocus@hotmail.com>
2020-07-07 17:46:55 +0100

    homework exercise

---
+++ exercise_calc.py
@@ -0,0 +1,12 @@
+import math
+
+
+class Functions:
+
+    @staticmethod
+    def find_sqrt(num):
+        return math.sqrt(num)
+
+    @staticmethod
+    def find_ceil(num):
+        return math.ceil(num)
---
+++ testing_exercise.py
@@ -0,0 +1,23 @@
+import unittest
+import math
+from exercise_calc import Functions
+
+
+
+class Calc_Test(unittest.TestCase):
+
+    a = Functions()
+
+    def test_sqrt(self):
+        self.assertEqual(self.a.find_sqrt(16), 4)
+
+    def test_find_ceil(self):
+        self.assertEqual(self.a.find_ceil(25.8237), 26)
+
+
+
+
+b = Functions
+print(b.find_sqrt(16))
+
+print(b.find_ceil(25.98450754))
